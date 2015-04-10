736952cb881b5ae04bcafe2dad5bf13656051c08
josephernest <nouvellecollection@gmail.com>
2015-04-10 18:32:48 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php 
+
+$sitename = "SomeWebsite";
+$blogpagename = "blog";
+
+error_reporting(0);
+
+function getpage($page)
+{
+  $pagestr = file_get_contents($page);
+  list($pageheader, $pagecontent) = preg_split('~(?:\r?\n){2}~', $pagestr, 2);  // split into 2 parts : before/after the first blank line
+  
+  preg_match("/^TITLE:(.*)$/m", $pageheader, $matches1);                        // for articles: title, for pages: title displayed in top-menu
+  preg_match("/^AUTHOR:(.*)$/m", $pageheader, $matches2);                       // for articles only
+  preg_match("/^DATE:(.*)$/m", $pageheader, $matches3);                         // for articles only
+  preg_match("/^(NOMENU:1)$/m", $pageheader, $matches4);                        // for pages only: if NOMENU:1, no link in top-menu
+  preg_match("/^URL:(.*)$/m", $pageheader, $matches5);                          // for pages only: top-menu's link  (=TITLE if no URL is set)
+  return array($pageheader, $pagecontent, $matches1[1], $matches2[1], $matches3[1], $matches4[1], (isset($matches5[1]) ? $matches5[1] : strtolower($matches1[1])));
+}
+
+$requestedpage = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+if (trim(dirname(parse_url($_SERVER['PHP_SELF'], PHP_URL_PATH)), '/') === $requestedpage) { $requestedpage = ""; }     // check if page is home, there should be a better way to do it!
+$type =  strpos($_SERVER['REQUEST_URI'], 'article') ? 'article' : 'page';
+$pages = glob("./" .$type."/*$requestedpage.txt");
+if ($pages) { $page = $pages[0]; } else { $page = "./page/HIDDEN-404.txt"; $type = 'page'; }                 // default 404 error page
+list($pageheader, $pagecontent, $pagetitle, $pageauthor, $pagedate, $pagenomenu, $pageurl) = getpage($page);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Void<?php echo (!empty(trim($pagetitle)) ? " - $pagetitle" : "")?></title>
+  <base href="<?php echo rtrim(dirname(parse_url($_SERVER['PHP_SELF'], PHP_URL_PATH)), '/') . '/'; ?>">  
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="header">
+  <div class="logo"><a href="."><?php echo $sitename;?></a></div>
+  <ul class="menu">
+    <?php
+    $pages = glob("./page/*.txt");
+    foreach($pages as $page)
+    {
+      list($menupageheader, $menupagecontent, $menupagetitle, $menupageauthor, $menupagedate, $menupagenomenu, $menupageurl) = getpage($page);
+      if (!$menupagenomenu) { echo "<li><a href=\"$menupageurl\">$menupagetitle</a></li>"; }
+    }
+    ?>      
+  </ul>
+</div>
+<div class="main">
+
+<?php
+require 'Parsedown.php';
+
+if ($type === "article") { echo "<h2 class=\"articletitle\">$pagetitle</h2><div class=\"articleinfo\">by $pageauthor, on $pagedate</div>"; } 
+
+echo (new Parsedown())->text($pagecontent);
+
+if ($requestedpage === $blogpagename)
+{
+  $pages = array_slice(array_reverse(glob("./article/*.txt")), $_GET['start'], 10);
+  foreach($pages as $page)
+  {
+    list($pageheader, $pagecontent, $pagetitle, $pageauthor, $pagedate, $pagenomenu, $pageurl) = getpage($page);
+    echo "<div class=\"article\"><a href=\"". substr($page, 2, strlen($page)-6) . "\">";
+    echo "<h2 class=\"articletitle\">$pagetitle</h2><div class=\"articleinfo\">by $pageauthor, on $pagedate</div></a>";
+    echo (new Parsedown())->text($pagecontent);
+    echo "</div>";
+  }
+  if ($_GET['start'] > 0) { echo "<a href=\"" . $blogpagename . (($_GET['start'] > 10) ? "?start=" . ($_GET['start'] - 10) : "") . "\">Newer articles</a>&nbsp; "; }
+  if (count(array_slice(array_reverse(glob("./article/*.txt")), $_GET['start'], 11)) > 10) { echo "<a href=\"" . $blogpagename . "?start=" . ($_GET['start'] + 10) . "\">Older articles</a>"; }
+}
+
+?>
+
+</div>
+<div class="footer">
+  <div class="left"><a href="">© 2015 <?php echo $sitename?></a></div>
+  <div class="right">Powered by <a href="http://www.thisisvoid.org">Void</a>.</div>
+</div>
+</body>
+</html>
